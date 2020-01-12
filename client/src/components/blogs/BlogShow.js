fd9d4474fb0c3a3a9cf3f730b4e14c959a421898
Rashad Ibrahimov <i.redjee@gmail.com>
2020-01-12 20:35:10 +0400

    Add image alt

--- client/src/components/blogs/BlogShow.js
+++ client/src/components/blogs/BlogShow.js
@@ -10,7 +10,9 @@ class BlogShow extends Component {
   renderImage() {
     if (this.props.blog.imageUrl) {
       return (
-        <img src={`https://udemy-nodejs-advanced-blog.s3.amazonaws.com/${this.props.blog.imageUrl}`} />
+        <img
+          src={`https://udemy-nodejs-advanced-blog.s3.amazonaws.com/${this.props.blog.imageUrl}`}
+          alt="blog image" />
       );
     }
   }
