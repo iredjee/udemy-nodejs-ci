8c708642cdc61a73a90ae9d15e875b887be4549d
Rashad Ibrahimov <i.redjee@gmail.com>
2020-01-12 20:30:45 +0400

    Fix error

--- client/src/components/blogs/BlogShow.js
+++ client/src/components/blogs/BlogShow.js
@@ -9,10 +9,9 @@ class BlogShow extends Component {
 
   renderImage() {
     if (this.props.blog.imageUrl) {
-      return <img src={
-        'https://udemy-nodejs-advanced-blog.s3.amazonaws.com/' +
-        this.props.blog.imageUrl
-      } />;
+      return (
+        <img src={`https://udemy-nodejs-advanced-blog.s3.amazonaws.com/${this.props.blog.imageUrl}`} />
+      );
     }
   }
 
