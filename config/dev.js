e1a9a9be995bec9fd1e47e7da470fe36a9369b5a
Rashad Ibrahimov <i.redjee@gmail.com>
2020-01-12 20:01:07 +0400

    Update configs

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  googleClientID:
-    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://udemy:tG7rpMXdBLolw8kR@udemy-nodejs-advanced-shard-00-00-wmt4x.mongodb.net:27017,udemy-nodejs-advanced-shard-00-01-wmt4x.mongodb.net:27017,udemy-nodejs-advanced-shard-00-02-wmt4x.mongodb.net:27017/test?ssl=true&replicaSet=udemy-nodejs-advanced-shard-0&authSource=admin&retryWrites=true&w=majority',
-  redisUrl: 'redis://127.0.0.1:6379',
-  cookieKey: '123123123'
-};
--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,7 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   redisUrl: process.env.REDIS_URL,
-  cookieKey: process.env.COOKIE_KEY
+  cookieKey: process.env.COOKIE_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 };
